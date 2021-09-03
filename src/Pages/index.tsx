ba4a281f46c29747c4410c1bf31bfd06d7c4cee1
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2021-09-03 18:06:07 +0300

    feat (catalog) add catalog and create request

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,12 +1,33 @@
-import { Layout } from "../Components/Common"
+import { useState, useEffect } from 'react';
+import { Layout } from '../Components/Common';
 
-
-const Index:React.FC = () => {
-  return (
-    <Layout>
-      <h2>index</h2>
-    </Layout>
-  )
+interface ICatalog {
+    title: string;
+    data: {
+        title: string;
+        price: string | null;
+        oldPrice: string | null;
+        image: string | null;
+        status: true;
+    }[];
 }
+
+const Index: React.FC = () => {
+    const [catalog, setCatalog] = useState<ICatalog>();
+
+    useEffect(() => {
+        const response = () => fetch(`http://localhost:3000/data/catalog.json`);
+        response()
+            .then((response) => response.json())
+            .then((data) => setCatalog(data))
+            .catch((error) => console.error(error));
+    }, []);
+
+    return (
+        <Layout>
+            <h2>index</h2>
+        </Layout>
+    );
+};
 
 export default Index;
